455e14a68714c9ded9140342ead06957d43b53d2
rahulrsavant <rahul.rsavant@gmail.com>
2026-01-21 15:32:20 +0100

    ocntext path aware changes

--- frontend_react/src/utils/apiConfig.js
+++ frontend_react/src/utils/apiConfig.js
@@ -11,7 +11,7 @@ const computeDefaultBaseUrl = () => {
     }
   }
 
-  return 'http://localhost:8080';
+  return 'http://localhost:8080/saiguru_industries';
 };
 
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || computeDefaultBaseUrl();
